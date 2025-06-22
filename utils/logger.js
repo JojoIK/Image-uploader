da81540931e5375430dca5b8127bde4a68321939
Joan Ikwen <joanikwen@gmail.com>
2025-06-22 01:26:50 +0100

    update all modules with logger utility

---
+++ utils/logger.js
@@ -0,0 +1,32 @@
+const winston = require("winston");
+require("dotenv").config();
+
+const logger = winston.createLogger({
+    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.json()
+    ),
+    defaultMeta: { service: 'Image Uploader' },
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({ filename: 'error.log', level: 'error', handleExceptions: true }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+    exitOnError: false
+})
+
+process.on('exit', () => {
+    if (logger.end) {
+        logger.end()
+    }
+})
+
+module.exports = logger
